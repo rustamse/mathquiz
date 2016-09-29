58565edb1807872f281a6288769403d042a26a2d
rustamse <rustamse@gmail.com>
2016-09-29 20:00:31 +0500

    Одржимость элементарным типом: массив key ну совсем уж неверно использовался.

--- mathquiz.js
+++ mathquiz.js
@@ -1,36 +1,41 @@
 "use strict";
 
-var key = [];
+var key = {
+    operator: "+",
+    leftValue: 0,
+    rightValue: 0,
+    expectedResult: 0
+};
 var score = 0;
 
 generateNewQuestion(key);
 
 function generateNewQuestion(legend) {
-    legend[0] = Math.random() > 0.5 ? "+" : "-";
+    legend.operator = Math.random() > 0.5 ? "+" : "-";
 
-    if (legend[0] == "+") {
-        legend[1] = Math.floor(Math.random() * 20) + 1;
-        legend[2] = Math.floor(Math.random() * 20) + 1;
-        legend[3] = legend[1] + legend[2];
+    if (legend.operator == "+") {
+        legend.leftValue = Math.floor(Math.random() * 20) + 1;
+        legend.rightValue = Math.floor(Math.random() * 20) + 1;
+        legend.expectedResult = legend.leftValue + legend.rightValue;
         printQuestion(legend);
         return legend;
     } else {
-        legend[1] = Math.floor(Math.random() * 30) + 10;
-        legend[2] = Math.floor(Math.random() * legend[1]) + 1;
-        legend[3] = legend[1] - legend[2];
+        legend.leftValue = Math.floor(Math.random() * 30) + 10;
+        legend.rightValue = Math.floor(Math.random() * legend.leftValue) + 1;
+        legend.expectedResult = legend.leftValue - legend.rightValue;
         printQuestion(legend);
         return legend;
     }
 }
 
-function printQuestion(output) {
-    document.getElementById("question").innerHTML = key[1] + " " + key[0] + " " + key[2] + " = ";
+function printQuestion(legend) {
+    document.getElementById("question").innerHTML = legend.leftValue + " " + legend.operator + " " + legend.rightValue + " = ";
     document.getElementById("field").value = "";
 }
 
 function onSubmitClick() {
     var answer = document.getElementById("field").value;
-    if (answer == key[3]) {
+    if (answer == key.expectedResult) {
         document.getElementById("check").innerHTML = "CORRECT! GOOD JOB! NOW TRY THIS ONE.";
         score += 1;
         if (score == 10) {
